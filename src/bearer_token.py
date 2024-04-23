2e6bec181b658f41c4c60a033d1f1574f9bc1cb5
lavaqq <mlava@tuta.io>
2024-04-23 09:59:00 +0200

    feat(bearer_token): error handling

--- src/bearer_token.py
+++ src/bearer_token.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 def get(username, password, api_url):
     auth_endpoint = api_url + '/login'
@@ -9,12 +10,24 @@ def get(username, password, api_url):
     try:
         response = requests.post(auth_endpoint, json=payload)
         response.raise_for_status()
-        bearer_token = response.json().get('access_token')
+        bearer_token = response.json().get('token')
         if bearer_token:
             return bearer_token
         else:
-            print("Failed to obtain Bearer token.")
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return None
+            print("Bearer token not found in response.")
+            sys.exit(1)
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        sys.exit(1)
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f'Connection error occurred: {conn_err}')
+        sys.exit(1)
+    except requests.exceptions.Timeout as timeout_err:
+        print(f'Timeout error occurred: {timeout_err}')
+        sys.exit(1)
+    except requests.exceptions.RequestException as req_err:
+        print(f'An error occurred: {req_err}')
+        sys.exit(1)
+    except ValueError as json_err:
+        print(f'JSON decoding failed: {json_err}')
+        sys.exit(1)
