e3f368a6224813eff97581fcb575603d656ecb46
lavaqq <mlava@tuta.io>
2024-04-23 12:37:19 +0200

    fix: typo & import

--- src/bearer_token.py
+++ src/bearer_token.py
@@ -1,11 +1,12 @@
 import requests
-import sys
+import os
 import json
+import sys 
 
 def get(username, password, api_url):
     auth_endpoint = api_url + '/login'
     headersList = {
-    "Content-Type": "application/json" 
+        "Content-Type": "application/json"
     }
     payload = json.dumps({
         "username": username,
@@ -14,24 +15,13 @@ def get(username, password, api_url):
     try:
         response = requests.post(auth_endpoint, data=payload, headers=headersList)
         response.raise_for_status()
-        bearer_token = response.json().get('token')
-        if bearer_token:
-            return bearer_token
+        data = response.json()
+        token = data.get('token')
+        if token:
+            return token
         else:
-            print("Bearer token not found in response.")
+            print("No token found in the response.")
             sys.exit(1)
     except requests.exceptions.HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
-        sys.exit(1)
-    except requests.exceptions.ConnectionError as conn_err:
-        print(f'Connection error occurred: {conn_err}')
-        sys.exit(1)
-    except requests.exceptions.Timeout as timeout_err:
-        print(f'Timeout error occurred: {timeout_err}')
-        sys.exit(1)
-    except requests.exceptions.RequestException as req_err:
-        print(f'An error occurred: {req_err}')
-        sys.exit(1)
-    except ValueError as json_err:
-        print(f'JSON decoding failed: {json_err}')
         sys.exit(1)
