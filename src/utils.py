6fa307dd1746ebaba4089228ccc5ca3491f6d200
lavaqq <mlava@tuta.io>
2024-04-24 11:20:53 +0200

    feat(utils): quantities fraction conversion and replace "," to "."

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import sys
 from datetime import date, datetime, timedelta
+from fraction import Fraction
 
 def format_data(data):
     print('format')
@@ -9,3 +10,13 @@ def calculate_end_date(start_date, days):
     return date.strftime('%Y-%m-%d')
 
     
+def convert_quantity(quantity_str):
+    quantity_str = quantity_str.replace(',', '.')
+    try:
+        if '/' in quantity_str:
+            quantity = float(Fraction(quantity_str))
+        else:
+            quantity = float(quantity_str)
+        return str(quantity)
+    except ValueError:
+        return quantity_str
