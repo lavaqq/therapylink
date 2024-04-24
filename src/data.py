aec813dd72e8949b411f4ff38fd9afd9cc081e3f
lavaqq <mlava@tuta.io>
2024-04-24 08:45:43 +0200

    feat(data:get): api call to get data

---
+++ src/data.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import json
+from datetime import datetime
+
+def get(api_url, token, start_date, end_date):
+    auth_endpoint = api_url + '/therapylink'
+    headersList = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json" 
+    }
+    if isinstance(start_date, datetime):
+        start_date = start_date.isoformat()
+    if isinstance(end_date, datetime):
+        end_date = end_date.isoformat()
+    payload = json.dumps({
+        "date_from": start_date,
+        "date_to": end_date,
+    })
+    try:
+        response = requests.get(auth_endpoint, data=payload, headers=headersList)
+        response.raise_for_status()
+        data = response.json()
+        if data:
+            return data
+        else:
+            print("Data not found in response.")
+            sys.exit(1)
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        sys.exit(1)
